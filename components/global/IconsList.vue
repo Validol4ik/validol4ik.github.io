44b6d3c6a31d55d8e9155171a78d4987a2e36def
validol4ik <validol4iknew@gmail.com>
2021-11-01 17:49:45 +0200

    updated social list

--- components/global/IconsList.vue
+++ components/global/IconsList.vue
@@ -1,103 +1,47 @@
 <template>
-  <ul class="icons-list">
-    <li class="icons-item">
-      <a
-        target="_blank"
-        href="https://www.linkedin.com/in/valentyn-polishchyk-8a2435217/"
-      >
-        <icon-base>
-					<lin-icon />
-				</icon-base>
-      </a>
-    </li>
-		<li class="icons-item">
-      <a
-        target="_blank"
-        href="https://www.instagram.com/validol4ik12/"
-      >
-        <icon-base>
-					<insta-icon />
-				</icon-base>
-      </a>
-    </li>
-		<li class="icons-item">
-      <a
-        target="_blank"
-        href="https://t.me/valiidol4ik"
-      >
-        <icon-base>
-					<telega-icon />
-				</icon-base>
-      </a>
-    </li>
+  <ul class="soc-links">
+    <vs-button href="https://t.me/valiidol4ik" blank icon size="large" danger>
+      <i class="bx bxl-telegram"></i>
+    </vs-button>
+    <vs-button href="https://www.instagram.com/valentynpolishchyk" blank icon size="large" danger>
+      <i class="bx bxl-instagram-alt"></i>
+    </vs-button>
+    <vs-button
+      href="https://www.linkedin.com/in/valentyn-polishchyk-8a2435217/"
+      blank
+      icon
+      size="large"
+      danger
+    >
+      <i class="bx bxl-linkedin"></i>
+    </vs-button>
+    <vs-button href="https://github.com/validol4ik" blank icon size="large" danger>
+      <i class="bx bxl-github"></i>
+    </vs-button>
   </ul>
 </template>
 
 <script>
-import IconBase from "@/components/global/icons/IconBase";
-import TelegaIcon from "@/components/global/icons/Telega";
-import InstaIcon from "@/components/global/icons/Insta";
-import LinIcon from "@/components/global/icons/LinIcon";
-
 export default {
-  name: "IconsList",
-  components: {
-    IconBase,
-    TelegaIcon,
-		InstaIcon,
-		LinIcon
-  },
-};
+  name: 'IconsList'
+}
 </script>
 
 <style lang="scss">
-$iconsHover: #b02851;
-$iconsHoverFill: #fff;
-$iconsBorder: #b02851;
-$iconsFill: #000;
-
-@media (min-width: 1199px) {
-  .icons-item {
+.soc-links {
+  display: flex;
+  align-items: center;
+  .vs-button {
+    i {
+      font-size: 24px;
+      color: #000;
+      transition: 0.2s;
+    }
     &:hover {
-      background: $iconsHover;
-      path {
-        fill: $iconsHoverFill;
+      i {
+        color: #fff;
       }
     }
-  }
-}
-.icons-list {
-  display: flex;
-  align-items: center;
-}
-.icons-item {
-  margin-left: 13px;
-  width: 42px;
-  height: 42px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 2px solid $iconsBorder;
-  border-radius: 17px;
-  cursor: pointer;
-  transition: 0.2s ease-in-out;
-  a {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 42px;
-  }
-  svg {
-    transition: 0.2s ease-in-out;
-		path {
-			fill: $iconsFill;
-    }
-  }
-}
-@media (max-width: 767px) {
-  .icons-list {
-    margin-top: 30px;
   }
 }
 </style>
