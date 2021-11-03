5b377657ff6409ec34a6868be4873207702d29d3
validol4ik <validol4iknew@gmail.com>
2021-11-03 17:51:57 +0200

    added pages switcher to site header

--- components/header/PortfolioHeader.vue
+++ components/header/PortfolioHeader.vue
@@ -1,7 +1,13 @@
 <template>
   <header class="header">
     <div class="container">
-      <portfolio-profile />
+      <div class="pseudo-nav">
+        <portfolio-profile />
+        <vs-select v-model="page" class="page-selector" placeholder="Select Page">
+          <vs-option label="Home" value="home">Home</vs-option>
+          <vs-option label="About Me" value="about">About Me</vs-option>
+        </vs-select>
+      </div>
       <site-settings />
     </div>
   </header>
@@ -16,6 +22,29 @@ export default {
   components: {
     PortfolioProfile,
     SiteSettings
+  },
+  data() {
+    return {
+      page: 'home'
+    }
+  },
+  watch: {
+    page(value) {
+      if (value === 'home') this.$router.push({ path: '/' })
+      else if (value === 'about') this.$router.push({ path: '/about' })
+    },
+    $route(value) {
+      switch (value.name) {
+        case 'index':
+          this.page = 'home'
+          break
+        case 'about':
+          this.page = 'about'
+          break
+        default:
+          this.page = ''
+      }
+    }
   }
 }
 </script>
@@ -31,6 +60,14 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+  .pseudo-nav {
+    display: flex;
+    align-items: center;
+  }
+  .page-selector {
+    max-width: 130px;
+    margin-left: 30px;
   }
 }
 @media (max-width: 767px) {
