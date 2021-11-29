ab3457448173d7a12704674e40f01293958640a6
validol4ik <validol4iknew@gmail.com>
2021-11-29 21:54:23 +0200

    upgraded about me section on about me page

--- components/aboutPage/AboutSection.vue
+++ components/aboutPage/AboutSection.vue
@@ -3,18 +3,15 @@
     <div class="container">
       <h4 class="pretitle">About me</h4>
       <div class="flex-wrapper">
-        <div class="about-media">
-          <img :src="imgSrc" alt="me" />
-          <div class="layer"></div>
-        </div>
+        <img src="@/assets/img/about.webp" class="portret" alt="me" />
         <div class="bio">
-          <h1 class="bio-name">
-            <span class="error">Valentyn</span>
+          <h1 class="bio-full-name">
+            <span class="bio__title bio-name">Valentyn</span>
             <br />
-            <span class="error">Polishchyk</span>
+            <span class="bio__title bio-last-name">Polishchyk</span>
           </h1>
           <p
-            class="whatiam"
+            class="bio__desc"
           >Proin imperdiet lacus quis odio aliquet, commodo scelerisque libero molestie. Duis fermentum nisi ut semper accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id volutpat enim, quis malesuada metus. Ut lobortis urna maximus.</p>
         </div>
       </div>
@@ -24,12 +21,7 @@
 
 <script>
 export default {
-  name: 'AboutSection',
-  computed: {
-    imgSrc() {
-      return require(`@/assets/img/about.webp`)
-    }
-  }
+  name: 'AboutSection'
 }
 </script>
 
@@ -38,39 +30,19 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-.about-media {
-  width: 32%;
+.portret {
+  width: 25%;
   position: relative;
-  img {
-    max-width: 80%;
-    border-radius: 16px;
-    // box-shadow: 50px -10px 75px rgba($color: #e34a57, $alpha: 0.6);
-  }
-  .layer {
-    position: absolute;
-    border-radius: 16px;
-    right: 0;
-    top: 0;
-    width: 60%;
-    height: 80%;
-    // z-index: -1;
-    background: rgba($color: #e34a57, $alpha: 0.2);
-  }
+  border-radius: 16px;
 }
+
 .bio {
   width: 58%;
-  .whatiam {
+  .bio-full-name {
     margin-top: 40px;
-    font-size: 20px;
-    line-height: 34px;
-    letter-spacing: 1px;
-    padding-right: 80px;
+    line-height: 65px;
   }
-}
-.bio-name {
-  margin-top: 40px;
-  line-height: 65px;
-  span.error {
+  &__title {
     font-family: 'Montserrat', sans-serif;
     font-size: 48px;
     letter-spacing: 12px;
@@ -92,6 +64,13 @@ export default {
       width: 106%;
       background: rgba($color: #e34a57, $alpha: 0.6);
     }
+  }
+  &__desc {
+    margin-top: 40px;
+    font-size: 20px;
+    line-height: 34px;
+    letter-spacing: 1px;
+    padding-right: 80px;
   }
 }
 </style>
